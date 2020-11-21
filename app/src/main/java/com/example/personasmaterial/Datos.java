cdcc6565e62b6edaa1c73da10cb4760900066783
Daniela Toncel Molina <daniela.toncel.ext@kavak.com>
2020-11-21 15:02:18 -0500

    Crear Personas terminado sin validaciones

--- app/src/main/java/com/example/personasmaterial/Datos.java
+++ app/src/main/java/com/example/personasmaterial/Datos.java
@@ -1,13 +1,24 @@
 package com.example.personasmaterial;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.sql.DatabaseMetaData;
 import java.util.ArrayList;
 
 public class Datos {
+    private static String db = "Personas";
+    private static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     public static ArrayList<Persona> personas = new ArrayList();
 
-    public static void guardar(Persona p){
-        personas.add(p);
+    public static String getId(){
+        return databaseReference.push().getKey();
     }
+
+    public static void guardar(Persona p){
+        databaseReference.child(db).child(p.getId()).setValue(p);
+    }
+
     public static ArrayList<Persona> obtener(){
         return personas;
     }
--- app/src/main/java/com/example/personasmaterial/DetallePersona.java
+++ app/src/main/java/com/example/personasmaterial/DetallePersona.java
@@ -2,13 +2,40 @@ package com.example.personasmaterial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetallePersona extends AppCompatActivity {
+    private TextView cedula, nombre, apellido;
+    private ImageView foto;
+    private Intent intent;
+    private Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_persona);
+
+        String ced, nom, apell;
+        foto = findViewById(R.id.imgFotoDetalle);
+        cedula = findViewById(R.id.txtCedulaDetalle);
+        nombre = findViewById(R.id.txtNombreDetalle);
+        apellido = findViewById(R.id.txtApellidoDetalle);
+
+        intent = getIntent();
+        bundle = intent.getBundleExtra("datos");
+
+        ced = bundle.getString("cedula");
+        nom = bundle.getString("nombre");
+        apell = bundle.getString("apellido");
+
+        cedula.setText(ced);
+        nombre.setText(nom);
+        apellido.setText(apell);
+
+
     }
 }
